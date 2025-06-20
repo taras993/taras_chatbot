d6ad8befaba77480d9abe1d3253eee8c437fe185
taras993 <tarasbezushko21@gmail.com>
2025-06-20 10:54:51 -0400

    added train data

--- bot.py
+++ bot.py
@@ -1,8 +1,20 @@
 # bot.py
 
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
 
 chatbot = ChatBot("Chatpot")
+
+trainer = ListTrainer(chatbot)
+trainer.train([
+    "Hi",
+    "Welcome, friend 🤗",
+])
+trainer.train([
+    "Are you a plant?",
+    "No, I'm the pot below the plant!",
+])
 
 exit_conditions = (":q", "quit", "exit")
 while True:
